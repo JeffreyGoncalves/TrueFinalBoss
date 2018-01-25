94ae602807c1d328e6c8b8241fe4b275747837cc
FloVollmer <vollmerflorent@gmail.com>
2018-01-25 17:55:50 +0100

    debut code

--- code.c
+++ code.c
@@ -1,8 +1,41 @@
-#include <string.h>
 #include <stdio.h>
 #include "tp.h"
 #include "tp_y.h"
+#include <fcntl.h>
+
+void makeCode();
+
 
 extern char* strdup(const char *);
 
 
+int main(int argc, char **argv) {
+	
+	printf("Ecriture du code\n");
+	
+	makeCode();
+	
+	
+	return 0;
+}
+
+
+
+
+void makeCode() {/*TreeP* tree*/
+	
+	FILE * pFile;
+	pFile = fopen ("myfile.txt","w");
+	
+	if (pFile!=NULL) {
+		fputs ("fopen example",pFile);
+		fclose (pFile);
+	}
+	
+	/*switch(op) {
+		case SUM :
+		
+		break;
+	}*/
+	
+}
