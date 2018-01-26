d0d99a338d89aa2572810a8e2023cf224cec5161
FloVollmer <vollmerflorent@gmail.com>
2018-01-26 10:43:02 +0100

    Sommes et produits

--- code.c
+++ code.c
@@ -8,34 +8,55 @@ void makeCode();
 
 extern char* strdup(const char *);
 
+/*int main(int argc, char **argv) {
 
-/* int main(int argc, char **argv) {
-	
-	printf("Ecriture du code\n");
-	
-	makeCode();
-	
-	
-	return 0;
-} */
+	printf("Construction de l'arbre de test\n");
 
+    TreeP tree = makeNode(2, SUM);
+    setChild(tree, 0, makeLeafInt(CST, 2));
+    setChild(tree, 1, makeNode(2, MULT));
+    setChild(getChild(tree, 1), 0, makeLeafInt(CST, 3));
+    setChild(getChild(tree, 1), 1, makeLeafInt(CST, 4));
 
-
-
-void makeCode() {/*TreeP* tree*/
-	
-	FILE * pFile;
+	FILE* pFile;
 	pFile = fopen ("myfile.txt","w");
-	
-	if (pFile!=NULL) {
-		fputs ("fopen example",pFile);
-		fclose (pFile);
+
+	printf("Ecriture du code\n");
+	makeCode(tree, pFile);
+
+    fclose (pFile);
+	return 0;
+}*/
+
+void makeCode(TreeP tree, FILE* pFile) {
+
+	if (pFile==NULL) {
+        printf("erreur fichier\n");
+        return;
 	}
-	
-	/*switch(op) {
+
+	switch(tree->op) {
 		case SUM :
-		
+        fputs (" -- Il y a une somme\n", pFile);
+        makeCode(tree->u.children[0], pFile);
+        makeCode(tree->u.children[1], pFile);
+        fputs ("ADD\n", pFile);
+        fputs (" -- Fin de la somme\n", pFile);
 		break;
-	}*/
-	
+		case MULT :
+        fputs (" -- Il y a un produit\n", pFile);
+        makeCode(tree->u.children[0], pFile);
+        makeCode(tree->u.children[1], pFile);
+        fputs ("MULT\n", pFile);
+        fputs (" -- Fin du produit\n", pFile);
+		break;
+		case CST :
+        fprintf ( pFile, "PUSHI %d\n", tree->u.val);
+		break;
+		default :
+        fputs (" -- Il y a quelque chose\n", pFile);
+		break;
+
+	}
+
 }
